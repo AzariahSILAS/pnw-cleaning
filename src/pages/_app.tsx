330116aeda888e5604926f425968c8f411d5e07a
unknown <azziesilas@gmail.com>
2025-06-14 12:55:08 -0700

    another take?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,35 +2,43 @@ import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import { FB_PIXEL_ID } from '@/lib/fbpixel';
 
+declare global {
+  interface Window {
+    fbq: (...args: unknown[]) => void;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Load Facebook Pixel script from Meta */}
+      {/* Load Facebook Pixel script */}
       <Script
         id="fb-pixel-loader"
         strategy="afterInteractive"
         src="https://connect.facebook.net/en_US/fbevents.js"
         onLoad={() => {
-          if (typeof window.fbq === 'undefined') {
-            window.fbq = function () {
+          if (!window.fbq) {
+            const fbq: (...args: unknown[]) => void = function (...args) {
               (window.fbq as any).callMethod
-                ? (window.fbq as any).callMethod.apply(window.fbq, arguments)
-                : (window.fbq as any).queue.push(arguments);
+                ? (window.fbq as any).callMethod(...args)
+                : (window.fbq as any).queue.push(args);
             };
-            (window.fbq as any).queue = [];
-            (window.fbq as any).loaded = true;
-            (window.fbq as any).version = '2.0';
-            (window.fbq as any).push = window.fbq;
+
+            fbq.queue = [];
+            fbq.loaded = true;
+            fbq.version = '2.0';
+            fbq.push = fbq;
+
+            window.fbq = fbq;
           }
 
           window.fbq('init', FB_PIXEL_ID);
           window.fbq('track', 'PageView');
 
-          console.log('✅ FB Pixel initialized');
+          console.log('✅ Facebook Pixel initialized');
         }}
       />
       <Component {...pageProps} />
     </>
   );
 }
-
